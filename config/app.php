509a80b1a11e68d70a439fafe359862f817d4729
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-05-22 14:50:38 -0700

    Add Jenssegers\Mongodb

--- config/app.php
+++ config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Other Providers...
+         */
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
     ],
 
     /*
--- config/database.php
+++ config/database.php
@@ -35,6 +35,18 @@ return [
 
     'connections' => [
 
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'host'     => env('MONGODB_HOST', 'localhost'),
+            'port'     => env('MONGODB_PORT', 27017),
+            'database' => env('MONGODB_DATABASE'),
+            'username' => env('MONGODB_USERNAME'),
+            'password' => env('MONGODB_PASSWORD'),
+            'options'  => [
+                'database' => 'admin', // sets the authentication database required by mongo 3
+            ],
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
