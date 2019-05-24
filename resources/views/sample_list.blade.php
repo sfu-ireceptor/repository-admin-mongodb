e5509abcc4bbccac8cc2a2a398f215ea34925eb5
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-05-24 13:07:41 -0700

    Use Bootstrap 4

--- resources/views/sample_list.blade.php
+++ resources/views/sample_list.blade.php
@@ -1,36 +1,44 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<title>Samples</title>
+	<title>Samples</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
-<table>
-	<thead>
-		<tr>
-			@foreach ($field_list as $field => $v)
-				<th class="text-nowrap">
-					{{ $field }}
-				</th>
-			@endforeach		
-		</tr>
-	</thead>
-	<tbody>
-		@foreach ($item_list as $item)
+<div class="container-fluid">
+	<h1>Samples</h1>
+	<table class="table table-bordered table-striped">
+		<thead class="thead-dark">
 			<tr>
 				@foreach ($field_list as $field => $v)
-					<td class="text-nowrap">
-						@isset($item[$field])
-							{{ $item[$field] }}
-						@endisset
-					</td>
-				@endforeach
+					<th class="text-nowrap">
+						{{ $field }}
+					</th>
+				@endforeach		
 			</tr>
-		@endforeach		
-	</tbody>
-</table>
+		</thead>
+		<tbody>
+			@foreach ($item_list as $item)
+				<tr>
+					@foreach ($field_list as $field => $v)
+						<td class="text-nowrap">
+							@isset($item[$field])
+								{{ $item[$field] }}
+							@endisset
+						</td>
+					@endforeach
+				</tr>
+			@endforeach		
+		</tbody>
+	</table>
+
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
